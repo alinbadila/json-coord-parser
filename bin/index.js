876ba08ff77a8a7c932175a17ce723a28d0803e6
Alin Badila <badila.alin@yahoo.com>
2023-04-04 19:14:53 +0300

    added jsonReader function

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
-console.log("Hello!");
+const fs = require("fs");
+const yargs = require("yargs");
+
+const options = yargs.usage("Usage: -f <json file>").option("f", {
+  alias: "filename",
+  describe: "path and name of the json input file",
+  type: "string",
+  demandOption: true,
+}).argv;
+
+console.log("Input file: ", options.filename);
+
+function jsonReader(filePath, cb) {
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      return cb && cb(err);
+    }
+    try {
+      const object = JSON.parse(fileData);
+      return cb && cb(null, object);
+    } catch (err) {
+      return cb && cb(err);
+    }
+  });
+}
+jsonReader(options.filename, (err, jsonMainObject) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(jsonMainObject.areas);
+});
